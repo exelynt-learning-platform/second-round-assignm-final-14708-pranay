ed4388f5387994350fc0a62f7001104d0ebb4ae1
Pranay Jumde <pranayjumde13@gmail.com>
2026-04-01 04:05:32 +0530

    Initial Commit: Setup Of models

---
+++ E-Commerce-Application/src/main/java/com/ecommerce/app/model/Product.java
@@ -0,0 +1,51 @@
+package com.ecommerce.app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product Name must contain at least 3 characters")
+    private String name;
+
+    @NotBlank
+    @Size(min = 6, message = "Product Description must contain at least 6 characters")
+    private String description;
+
+    private Integer stockQuantity;
+
+    private double price;
+
+    private double specialPrice;
+
+    private double discount;
+
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+}
