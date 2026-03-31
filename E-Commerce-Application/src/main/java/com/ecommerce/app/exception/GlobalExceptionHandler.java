851fa1847e49b5272ea8843176da9633089ac675
Pranay Jumde <pranayjumde13@gmail.com>
2026-04-01 04:09:38 +0530

    Added Global Exception Handler

---
+++ E-Commerce-Application/src/main/java/com/ecommerce/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.ecommerce.app.exception;
+
+import com.ecommerce.app.dto.response.APIResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> response = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            response.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<APIResponse> resourceNotFoundException(ResourceNotFoundException ex) {
+        String message = ex.getMessage();
+
+        APIResponse response = new APIResponse(message, false);
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<APIResponse> apiException(APIException ex) {
+        String message = ex.getMessage();
+        APIResponse response = new APIResponse(message, false);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
